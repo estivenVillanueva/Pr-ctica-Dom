1be22568771f0042ec3ed0a5aafdc5fb25fb13e8
estivenVillanueva <168735734+estivenVillanueva@users.noreply.github.com>
2024-11-21 20:29:52 -0500

    part two

---
+++ scripts/app.js
@@ -0,0 +1,37 @@
+import { countries } from "./countries.js"
+
+
+const makeCountry=(country)=>{
+    const container =document.createElement('div')
+    container.classList.add('card-country')
+
+    container.addEventListener('mouseenter',showInfo)
+    const imgContainer=document.createElement('img')
+    imgContainer.src=country.flag
+    imgContainer.alt=country.name
+
+    const nameContainer=document.createElement('h2')
+    nameContainer.textContent=country.name
+
+    container.appendChild(imgContainer)
+    container.appendChild(nameContainer)
+
+    document.querySelector('#section-countries').appendChild(container)
+}
+
+const renderCountries = () =>{
+    countries.forEach( country => makeCountry(country))
+}
+ window.addEventListener('DOMContentLoaded',renderCountries)
+
+ const showInfo=(event) =>{
+  const currentCountry=event.target.children[1].textContent
+  const infoCountry =countries.find(country=> country.name===currentCountry)
+  console.log(infoCountry)
+
+  const demographics = document.getElementById('demographics')
+  demographics.children[0].textContent=infoCountry.name
+  demographics.children[1].textContent=infoCountry.capital
+  demographics.children[2].textContent=infoCountry.population
+
+}
